c5dad68be84d726d06aabcf7b33b0dc495ba951a
PawanNayanajith <djnayanajith@gmail.com>
2019-03-26 00:33:38 +0530

    JumpingFrog Class Added

---
+++ JumpingFrog.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class JumpingFrog{
+
+	public static void main(String[] args){
+
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the Distance :  ");
+		int distance = input.nextInt();
+		
+		findTime(distance);
+
+	}
+
+	public static void findTime(int distance){
+
+		int travelledDistance = distance;
+		int[] distances = {5,3,2};
+		int[] times = {2,3,5};
+
+		int i = 0;
+		int totalTime = 0;
+
+		while(distance>0){
+			distance-=distances[i%3];
+			totalTime+=times[i%3];
+			i++;
+		}
+		System.out.println("Distance Travelled By Frog : " + travelledDistance + " Meters");
+		System.out.println("Total Time Taken By Frog : " + totalTime + " Seconds");
+	}
+}
