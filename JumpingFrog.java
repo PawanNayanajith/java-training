44a11102184b176493f788100682e52870fbb304
Pawan Nayanajith <djnayanajith@gmail.com>
2019-03-26 09:31:39 +0530

    Correction made & HopCount added.

--- JumpingFrog.java
+++ JumpingFrog.java
@@ -3,11 +3,10 @@ import java.util.Scanner;
 public class JumpingFrog{
 
 	public static void main(String[] args){
-
+        
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter the Distance :  ");
-		int distance = input.nextInt();
-		
+		int distance = input.nextInt();		
 		findTime(distance);
 
 	}
@@ -19,14 +18,19 @@ public class JumpingFrog{
 		int[] times = {2,3,5};
 
 		int i = 0;
-		int totalTime = 0;
+        	int totalTime = 0;
+       	 	int hopCount =0;
 
 		while(distance>0){
-			distance-=distances[i%3];
+		    	distance-=distances[i%3];
+		    	System.out.println(distance);
+		    	if(distance>0){
 			totalTime+=times[i%3];
-			i++;
+		    	}i++;
+		    	hopCount++;
 		}
-		System.out.println("Distance Travelled By Frog : " + travelledDistance + " Meters");
+        	System.out.println("Distance Travelled By Frog : " + travelledDistance + " Meters");
+        	System.out.println("No. of Hops : " + hopCount);
 		System.out.println("Total Time Taken By Frog : " + totalTime + " Seconds");
 	}
 }
